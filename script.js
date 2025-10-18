e09e5eebd044ae90ea1c42995f1e8626c40cda9b
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-18 18:13:48 +0530

    Add input and hint elements to script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const input_decimal = document.getElementById('input_decimal');
+const input_binary  = document.getElementById('input_binary');
+const input_octal   = document.getElementById('input_octal');
+const input_hex     = document.getElementById('input_hex');
 
+const hint_decimal = document.getElementById('hint_decimal');
+const hint_binary  = document.getElementById('hint_binary');
+const hint_octal   = document.getElementById('hint_octal');
+const hint_hex     = document.getElementById('hint_hex');
+
+const button_clear = document.getElementById('button_clear');
