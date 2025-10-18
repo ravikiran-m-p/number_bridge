34a805559be8d31ef95d445d4cbd328785ad32a7
MODALAVALASA RAVI KIRAN <ravikiranmodalavalasa@gmail.com>
2025-10-18 18:41:25 +0530

    Add regex patterns for number formats

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ const hint_hex     = document.getElementById('hint_hex');
 const button_clear = document.getElementById('button_clear');
 
 let is_updating = false;
+
+const re_decimal = /^-?\d+$/;
+const re_binary  = /^-?[01]+$/;
+const re_octal   = /^-?[0-7]+$/;
+const re_hex     = /^-?[0-9a-fA-F]+$/;
